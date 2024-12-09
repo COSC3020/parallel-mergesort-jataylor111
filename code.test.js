df4bd163750219c822e42c022674bf9a1340f7e1
jataylor111 <143449478+jataylor111@users.noreply.github.com>
2024-12-08 19:21:38 -0700

    Create code.test.js

---
+++ code.test.js
@@ -0,0 +1,19 @@
+// Note this test is borrowed from one caden mcfate
+const fs = require('fs');
+const jsc = require('jsverify');
+
+eval(fs.readFileSync('ParallelMerge.js')+'');
+
+var testSort =
+    jsc.forall("array nat", function(arr) {
+        var a1 = JSON.parse(JSON.stringify(arr));
+        var a2 = JSON.parse(JSON.stringify(arr));
+        var sync = a2.sort(function(a, b) {
+            return a - b; 
+        });
+        return mergesort(a1).then(() => {
+            return JSON.stringify(a1) === JSON.stringify(sync);
+        });
+    });
+
+jsc.check(testSort);
